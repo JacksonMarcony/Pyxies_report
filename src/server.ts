6862eb58461d80aa070f278ac78b8398faa5364b
Jackson Marcony <jacksonmarconyrs@gmail.com>
2022-05-24 10:32:15 -0300

    config of upload files

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import "reflect-metadata";
-import express from 'express';
+import express from "express";
 import "./infra/database";
+import morgan from "morgan";
 import { routes } from "./routes";
 
 const app = express();
-const PORT = 3000
+const PORT = 3000;
 
 app.use(express.json());
-
+app.use(morgan("dev"));
 app.use(routes);
 
-app.listen(`${PORT}`, () =>  console.log(`😋😋 Server is running in http://localhost:${PORT} 😋😋`));
+app.listen(`${PORT}`, () =>
+  console.log(`😋😋 Server is running in http://localhost:${PORT} 😋😋`)
+);
